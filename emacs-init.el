7b18f7bda934004c767453627ad2bea286d048e8
liuxiang <liuxiang@umeng.com>
2016-03-04 16:28:57 +0800

    Fix bug in case of that file path contains `~'

--- emacs-init.el
+++ emacs-init.el
@@ -129,6 +129,30 @@ it suitable for terminal.
           (slug (nth 1 parsed)))
       (cons domain (format "%s://%s/%s" proto domain slug)))))
 
+(with-eval-after-load "helm-files"
+  (defun helm-substitute-in-filename (fname)
+    (cond ((and ffap-url-regexp
+                (string-match-p ffap-url-regexp fname))
+          fname)
+          ((and (file-remote-p fname)
+                helm-substitute-in-filename-stay-on-remote)
+          (let ((sub (substitute-in-file-name fname)))
+            (if (file-directory-p sub)
+                sub (replace-regexp-in-string "/\\'" "" sub))))
+          (t
+          (with-temp-buffer
+            (insert fname)
+            (goto-char (point-min))
+            (skip-chars-forward "/") ;; Avoid infloop in UNC paths Issue #424
+            (if (re-search-forward "~/\\|//\\|/[[:alpha:]]:/" nil t)
+                (let ((match (match-string 0)))
+                  (goto-char (if (or (string= match "//")
+                                      (string-match-p "/[[:alpha:]]:/" match))
+                                  (1+ (match-beginning 0))
+                                  (match-beginning 0)))
+                  (buffer-substring-no-properties (point) (point-at-eol)))
+                fname))))))
+
 ;;;;;;; Advices ;;;;;;;;
 
 (advice-add 'magit-blame-format-time-string :override #'(lambda (format time tz)
