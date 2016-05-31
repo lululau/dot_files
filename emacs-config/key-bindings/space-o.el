e43ef22bd574f5d4407bf9c70e6a5382f70eb2e1
liuxiang <liuxiang@umeng.com>
2016-05-31 11:42:19 +0800

    Add space-o.el

---
+++ emacs-config/key-bindings/space-o.el
@@ -0,0 +1,4 @@
+(spacemacs/set-leader-keys
+  "oob" #'(lambda () (interactive) (find-file "~/Library/Mobile Documents/com~apple~CloudDocs/org/bookmarks.org"))
+  "oon" #'(lambda () (interactive) (find-file "~/Library/Mobile Documents/com~apple~CloudDocs/org/notes.org"))
+  )
