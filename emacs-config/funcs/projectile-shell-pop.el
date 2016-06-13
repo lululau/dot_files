bd1e80596c34bb5e4103a3366bd980e2138be7fd
liuxiang <lululau@me.com>
2016-06-13 22:00:56 +0800

    Fix bug

--- emacs-config/funcs/projectile-shell-pop.el
+++ emacs-config/funcs/projectile-shell-pop.el
@@ -3,11 +3,11 @@
   (if (string= "term-mode" major-mode)
       (shell-pop-out)
     (if (not (projectile-project-p))
-        (shell-pop-ansi-term 0)
+        (spacemacs/shell-pop-ansi-term 0)
       (if (not (boundp 'projectile-term-index-hash-table)) (setq projectile-term-index-hash-table (make-hash-table :test 'equal)))
       (let* ((projectile-project-root (projectile-project-root))
              (index (or (gethash projectile-project-root projectile-term-index-hash-table)
                         (1+ (apply 'max 99 (hash-table-values projectile-term-index-hash-table))))))
         (puthash projectile-project-root index projectile-term-index-hash-table)
-        (shell-pop-ansi-term index)))))
+        (spacemacs/shell-pop-ansi-term index)))))
 
