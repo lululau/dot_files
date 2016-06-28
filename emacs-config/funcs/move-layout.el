323b5c9a4f8db87edcfe03bdb6c5933130eb5fb6
liuxiang <liuxiang@ktjr.com>
2016-06-28 09:30:54 +0800

    Add support for move layout forward/backward

---
+++ emacs-config/funcs/move-layout.el
@@ -0,0 +1,32 @@
+(defun lx/swap-list-elem (list a b)
+  (let* ((elem_a (nth a list))
+         (elem_b (nth b list)))
+    (setcar (nthcdr a list) elem_b)
+    (setcar (nthcdr b list) elem_a)))
+
+(defun lx/next-list-index (list idx)
+  (if (= (1+ idx) (length list))
+      0
+    (1+ idx)))
+
+(defun lx/previous-list-index (list idx)
+  (if (= 0 idx)
+      (- (length list) 1)
+    (- idx 1)))
+
+(defun lx/current-layout-index ()
+  (-elem-index (spacemacs//current-layout-name) (persp-names-current-frame-fast-ordered)))
+
+(defun lx/move-layout-forward ()
+  (interactive)
+  (let* ((current (lx/current-layout-index))
+         (next (lx/next-list-index (persp-names-current-frame-fast-ordered) current)))
+    (lx/swap-list-elem (cdr (cdddr persp-minor-mode-menu)) current next))
+  (spacemacs/layouts-transient-state/body))
+
+(defun lx/move-layout-backward ()
+  (interactive)
+  (let* ((current (lx/current-layout-index))
+         (previous (lx/previous-list-index (persp-names-current-frame-fast-ordered) current)))
+    (lx/swap-list-elem (cdr (cdddr persp-minor-mode-menu)) current previous))
+  (spacemacs/layouts-transient-state/body))
---
+++ emacs-config/post-package-load/layouts.el
@@ -0,0 +1,75 @@
+ (spacemacs|use-package-add-hook persp-mode
+   :pre-init
+   (progn
+     (message "hello post-init-persp-mode")
+     (spacemacs|transient-state-format-hint layouts
+       spacemacs--layouts-ts-full-hint
+       "\n\n
+  Go to^^^^^^                                  Actions^^
+  ─────^^^^^^──────────────────────────────    ───────^^──────────────────────────────────────────────────
+  [_0_,_9_]^^     nth/new layout               [_a_]^^   add buffer
+  [_C-0_,_C-9_]^^ nth/new layout               [_A_]^^   add all from layout
+  [_<tab>_]^^^^   last layout                  [_d_]^^   close current layout
+  [_b_]^^^^       buffer in layout             [_D_]^^   close other layout
+  [_h_]^^^^       default layout               [_r_]^^   remove current buffer
+  [_l_]^^^^       layout w/helm/ivy            [_R_]^^   rename current layout
+  [_L_]^^^^       layouts in file              [_s_/_S_] save all layouts/save by names
+  [_n_/_C-l_]^^   next layout                  [_t_]^^   show a buffer without adding it to current layout
+  [_N_/_p_/_C-h_] prev layout                  [_x_]^^   kill current w/buffers
+  [_o_]^^^^       custom layout                [_X_]^^   kill other w/buffers
+  [_<_]^^^^       move layout backword         [_>_]^^   move layout forward
+  [_w_]^^^^       workspaces transient state   [_?_]^^   toggle help\n")
+
+     (spacemacs|define-transient-state layouts
+       :title "Layouts Transient State"
+       :hint-is-doc t
+       :dynamic-hint (spacemacs//layouts-ts-hint)
+       :bindings
+       ;; need to exit in case number doesn't exist
+       ("?" spacemacs//layouts-ts-toggle-hint)
+       ("1" spacemacs/persp-switch-to-1 :exit t)
+       ("2" spacemacs/persp-switch-to-2 :exit t)
+       ("3" spacemacs/persp-switch-to-3 :exit t)
+       ("4" spacemacs/persp-switch-to-4 :exit t)
+       ("5" spacemacs/persp-switch-to-5 :exit t)
+       ("6" spacemacs/persp-switch-to-6 :exit t)
+       ("7" spacemacs/persp-switch-to-7 :exit t)
+       ("8" spacemacs/persp-switch-to-8 :exit t)
+       ("9" spacemacs/persp-switch-to-9 :exit t)
+       ("0" spacemacs/persp-switch-to-0 :exit t)
+       ("C-1" spacemacs/persp-switch-to-1)
+       ("C-2" spacemacs/persp-switch-to-2)
+       ("C-3" spacemacs/persp-switch-to-3)
+       ("C-4" spacemacs/persp-switch-to-4)
+       ("C-5" spacemacs/persp-switch-to-5)
+       ("C-6" spacemacs/persp-switch-to-6)
+       ("C-7" spacemacs/persp-switch-to-7)
+       ("C-8" spacemacs/persp-switch-to-8)
+       ("C-9" spacemacs/persp-switch-to-9)
+       ("C-0" spacemacs/persp-switch-to-0)
+       ("<tab>" spacemacs/jump-to-last-layout)
+       ("<return>" nil :exit t)
+       ("C-h" persp-prev)
+       ("C-l" persp-next)
+       ("a" persp-add-buffer :exit t)
+       ("A" persp-import-buffers :exit t)
+       ("b" spacemacs/persp-helm-mini :exit t)
+       ("d" spacemacs/layouts-ts-close)
+       ("D" spacemacs/layouts-ts-close-other :exit t)
+       ("h" spacemacs/layout-goto-default :exit t)
+       ("l" spacemacs/helm-perspectives :exit t)
+       ("L" persp-load-state-from-file :exit t)
+       ("n" persp-next)
+       ("N" persp-prev)
+       ("o" spacemacs/select-custom-layout :exit t)
+       ("p" persp-prev)
+       ("r" persp-remove-buffer :exit t)
+       ("R" spacemacs/layouts-ts-rename :exit t)
+       ("s" persp-save-state-to-file :exit t)
+       ("S" persp-save-to-file-by-names :exit t)
+       ("t" persp-temporarily-display-buffer :exit t)
+       ("w" spacemacs/workspaces-transient-state/body :exit t)
+       ("x" spacemacs/layouts-ts-kill)
+       ("X" spacemacs/layouts-ts-kill-other :exit t)
+       (">" lx/move-layout-forward)
+       ("<" lx/move-layout-backward))))
