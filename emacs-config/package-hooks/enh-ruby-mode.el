dc3745bff23567516f4537ae82d5f6631c71a2cf
liuxiang <liuxiang@ktjr.com>
2016-06-29 12:02:34 +0800

    Fix bug

--- emacs-config/package-hooks/enh-ruby-mode.el
+++ emacs-config/package-hooks/enh-ruby-mode.el
@@ -1,14 +1,23 @@
+(with-eval-after-load 'enh-ruby-mode
+  (define-key enh-ruby-mode-map (kbd "s-r b") 'enh-ruby-toggle-block)
+  (define-key enh-ruby-mode-map (kbd "M-s-b") #'toggle-pry-breakpoint)
+  (define-key enh-ruby-mode-map (kbd "M-s-c") #'cleanup-pry-breakpoints)
+  (dolist (mode '(ruby-mode enh-ruby-mode))
+    (spacemacs/set-leader-keys-for-major-mode mode "TAB" 'rspec-toggle-spec-and-target)))
+
 (spacemacs|use-package-add-hook enh-ruby-mode
-  :post-config
+  :post-init
   (progn
-    (define-key enh-ruby-mode-map (kbd "s-r b") 'enh-ruby-toggle-block)
-    (define-key enh-ruby-mode-map (kbd "M-s-b") #'toggle-pry-breakpoint)
-    (define-key enh-ruby-mode-map (kbd "M-s-c") #'cleanup-pry-breakpoints)
-    (dolist (mode '(ruby-mode enh-ruby-mode)) (spacemacs/set-leader-keys-for-major-mode mode "TAB" 'rspec-toggle-spec-and-target))
-    (add-hook 'enh-ruby-mode-hook #'(lambda ()
-                                      (setq evil-shift-width 2)
-                                      (modify-syntax-entry ?: ".")
-                                      (modify-syntax-entry ?! "_")
-                                      (modify-syntax-entry ?? "_")))
-    (remove-hook 'enh-ruby-mode-hook
-                 (lambda () (rvm-activate-corresponding-ruby)))))
+    (add-hook
+     'enh-ruby-mode-hook
+     #'(lambda ()
+         (setq evil-shift-width 2)
+         (modify-syntax-entry ?: ".")
+         (modify-syntax-entry ?! "_")
+         (modify-syntax-entry ?? "_")))))
+
+(spacemacs|use-package-add-hook rvm
+  :post-init
+  (remove-hook
+   'enh-ruby-mode-hook
+   'rvm-activate-corresponding-ruby))
