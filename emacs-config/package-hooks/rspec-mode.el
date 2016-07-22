8e492abb097393ea365428b4e5729e2edcbe7294
liuxiang <liuxiang@ktjr.com>
2016-07-22 16:49:48 +0800

    Modify rspec-toggle-spec-and-target binding config

--- emacs-config/package-hooks/rspec-mode.el
+++ emacs-config/package-hooks/rspec-mode.el
@@ -1,3 +1,4 @@
 (spacemacs|use-package-add-hook rspec-mode
   :post-config
-  (define-key rspec-mode-map [M-s-tab] #'rspec-toggle-spec-and-target))
+  (define-key rspec-mode-map [M-s-tab] #'rspec-toggle-spec-and-target-find-example)
+  (define-key enh-ruby-mode-map [M-s-tab] #'rspec-toggle-spec-and-target-find-example))
