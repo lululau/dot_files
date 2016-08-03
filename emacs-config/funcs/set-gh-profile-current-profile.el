d1ed7f087c03017f8169293d281e254a91d3a323
liuxiang <liuxiang@ktjr.com>
2016-08-03 10:15:36 +0800

    Add monkey patches for github(gh and magit-gh-pulls) packages

---
+++ emacs-config/funcs/set-gh-profile-current-profile.el
@@ -0,0 +1,8 @@
+(defun lx/set-gh-profile-current-profile ()
+  (-each gh-profile-alist #'(lambda (e)
+                              (let ((regexp (plist-get (cdr e) :remote-regexp))
+                                    (profile (car e))
+                                    (remote-url (magit-get "remote" (magit-get-remote) "url")))
+                                (if (and regexp (string-match-p regexp remote-url))
+                                    (setq-local gh-profile-current-profile profile))))))
+
