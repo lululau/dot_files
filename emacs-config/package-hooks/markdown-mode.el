4b17c1801c71bfc465b0e6f880a04dcd56e3f7b6
liuxiang <liuxiang@ktjr.com>
2016-11-08 17:36:01 +0800

    Unset keys in markdown-mode-map

---
+++ emacs-config/package-hooks/markdown-mode.el
@@ -0,0 +1,9 @@
+(with-eval-after-load 'markdown-mode
+  (define-key markdown-mode-map (kbd "M-h") nil)
+  (define-key markdown-mode-map (kbd "M-j") nil)
+  (define-key markdown-mode-map (kbd "M-k") nil)
+  (define-key markdown-mode-map (kbd "M-l") nil)
+  (define-key markdown-mode-map (kbd "M-H") nil)
+  (define-key markdown-mode-map (kbd "M-J") nil)
+  (define-key markdown-mode-map (kbd "M-K") nil)
+  (define-key markdown-mode-map (kbd "M-L") nil))
