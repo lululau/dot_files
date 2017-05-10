292e21fe18f00d850580f62a7e4f4ad760179b72
Liu Xiang <liuxiang@ktjr.com>
2017-05-10 16:50:38 +0800

    Fix bug

--- emacs-config/package-hooks/dired-mode.el
+++ emacs-config/package-hooks/dired-mode.el
@@ -1,5 +1,7 @@
 (with-eval-after-load 'dired
   (define-key dired-mode-map (kbd "n") nil)
+  (define-key dired-mode-map (kbd "g") nil)
+  (define-key dired-mode-map (kbd "G") nil)
   (unless (or (display-graphic-p) (lx/system-is-linux))
       (defun dired-delete-file (file &optional recursive trash)
         (call-process "trash" nil nil nil file))))
