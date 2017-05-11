b26bee12554a78df51fca3a95e05cbca431e90b1
Liu Xiang <liuxiang@ktjr.com>
2017-05-11 15:01:28 +0800

    Fix bug

--- emacs-config/package-hooks/iedit.el
+++ emacs-config/package-hooks/iedit.el
@@ -1,6 +1,8 @@
 (with-eval-after-load 'iedit
   (add-hook 'iedit-mode-hook #'(lambda ()
-                                 (setq-local ggtags-mode-local-var ggtags-mode)
-                                 (if ggtags-mode-local-var (ggtags-mode -1))))
+                                 (when (boundp 'ggtags-mode)
+                                   (setq-local ggtags-mode-local-var ggtags-mode)
+                                   (if ggtags-mode-local-var (ggtags-mode -1)))))
   (add-hook 'iedit-mode-end-hook #'(lambda ()
-                                     (if ggtags-mode-local-var (ggtags-mode)))))
+                                     (when (boundp 'ggtags-mode)
+                                       (if ggtags-mode-local-var (ggtags-mode))))))
