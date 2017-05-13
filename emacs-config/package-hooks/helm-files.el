90167d50ecc79a4b07d8d6bdd708c21776fbcd42
Liu Xiang <liuxiang@ktjr.com>
2017-05-13 15:20:22 +0800

    Add helm-autojump

--- emacs-config/package-hooks/helm-files.el
+++ emacs-config/package-hooks/helm-files.el
@@ -1,5 +1,4 @@
-(spacemacs|use-package-add-hook helm-files
-  :post-config
+(with-eval-after-load 'helm-files
   (defun helm-substitute-in-filename (fname)
     (cond ((and ffap-url-regexp
                 (string-match-p ffap-url-regexp fname))
@@ -21,4 +20,34 @@
                                   (1+ (match-beginning 0))
                                 (match-beginning 0)))
                    (buffer-substring-no-properties (point) (point-at-eol)))
-               fname))))))
+               fname)))))
+
+  (defvar autojump-db-file (if (lx/system-is-mac)
+                               "~/Library/autojump/autojump.txt"
+                             "~/.local/share/autojump/autojump.txt"))
+
+  (defvar helm-autojump-command
+    (format "sort -n -r %s | cut -f2 | sed 's/$/\\//'" autojump-db-file))
+
+  (defun helm-autojump-candidates ()
+    (s-split "\n" (shell-command-to-string helm-autojump-command)))
+
+  (defvar helm-autojump-map (let ((map (make-sparse-keymap)))
+                              (set-keymap-parent map helm-find-files-map)
+                              (define-key map (kbd "RET") #'(lambda () (interactive)
+                                                                (helm-run-after-quit #'(lambda (sel)
+                                                                                         (helm-find-files-1 sel)) (helm-get-selection))))
+                              map))
+
+  (defclass helm-autojump-source (helm-source-ffiles)
+    ((candidates :initform 'helm-autojump-candidates)
+     (fuzzy-match :initform t)
+     (filtered-candidate-transformer :initform nil)
+     (keymap :initform helm-autojump-map)))
+
+  (defvar helm-source-autojump (helm-make-source "Autojump" 'helm-autojump-source))
+
+  (spacemacs/set-leader-keys "oj" #'(lambda () (interactive)
+                                      (helm :sources 'helm-source-autojump
+                                            :buffer "*helm autojump*"
+                                            :ff-transformer-show-only-basename nil))))
