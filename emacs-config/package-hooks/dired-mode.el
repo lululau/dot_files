17dd9872459ae75e8764095de4d8b664ba332381
Liu Xiang <liuxiang@ktjr.com>
2017-05-17 17:47:38 +0800

    Undefine "?" for dired-mode-map

--- emacs-config/package-hooks/dired-mode.el
+++ emacs-config/package-hooks/dired-mode.el
@@ -2,6 +2,7 @@
   (define-key dired-mode-map (kbd "n") nil)
   (define-key dired-mode-map (kbd "g") nil)
   (define-key dired-mode-map (kbd "G") nil)
+  (define-key dired-mode-map (kbd "?") nil)
   (unless (or (display-graphic-p) (lx/system-is-linux))
       (defun dired-delete-file (file &optional recursive trash)
         (call-process "trash" nil nil nil file))))
