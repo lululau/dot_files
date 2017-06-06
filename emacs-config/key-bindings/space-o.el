38cadbc9717e43e2199cb0313f29a1ea8cc72756
Liu Xiang <liuxiang@ktjr.com>
2017-06-06 11:05:37 +0800

    Misc config

--- emacs-config/key-bindings/space-o.el
+++ emacs-config/key-bindings/space-o.el
@@ -28,6 +28,7 @@
 ;; Demo files
 (setq lx/demo-files
       '(("oea" artist-demo  "~/Documents/org/demo/demo.art")
+        ("oeA" scala-demo   "~/cascode/github.com/prog-scala/src/main/scala/progscala2/demo.scala")
         ("oeh" http-demo    "~/Documents/org/demo/demo.http")
         ("oel" elisp-demo   "~/Documents/org/demo/demo.el")
         ("oer" ruby-demo    "~/Documents/org/demo/demo.rb")
