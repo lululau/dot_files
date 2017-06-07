2358de6ffa99469754b51e3475f88a8f3b51a7be
Liu Xiang <liuxiang@ktjr.com>
2017-06-07 11:24:47 +0800

    Fix bug

--- emacs-config/package-hooks/dired-mode.el
+++ emacs-config/package-hooks/dired-mode.el
@@ -5,6 +5,7 @@
   (define-key dired-mode-map (kbd "?") nil)
   (define-key dired-mode-map (kbd ")") 'dired-up-directory)
   (define-key dired-mode-map (kbd "-") 'dired-up-directory)
+  (define-key dired-mode-map (kbd "S-SPC") nil)
   (unless (or (display-graphic-p) (lx/system-is-linux))
       (defun dired-delete-file (file &optional recursive trash)
         (call-process "trash" nil nil nil file))))
