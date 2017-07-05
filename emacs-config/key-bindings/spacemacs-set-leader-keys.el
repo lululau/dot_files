921c13afa6b22e14ba2e1cec054e07872edc58f6
Liu Xiang <liuxiang@ktjr.com>
2017-07-05 11:25:44 +0800

    Revise magit status kbds

--- emacs-config/key-bindings/spacemacs-set-leader-keys.el
+++ emacs-config/key-bindings/spacemacs-set-leader-keys.el
@@ -4,8 +4,7 @@
   "bk" #'(lambda () (interactive) (call-interactively 'kill-buffer))
   "gC" 'magit-commit
   "gc" #'(lambda (arg) (interactive "P") (call-interactively (if arg 'magit-branch-and-checkout 'magit-checkout)))
-  "gS" 'magit-status
-  "gs" #'(lambda () (interactive) (magit-run-git-async "status") (magit-process-buffer))
+  "gS" #'(lambda () (interactive) (magit-run-git-async "status") (magit-process-buffer))
   "gu" #'(lambda () (interactive) (magit-checkout-file (magit-get-current-branch) (buffer-file-name)))
   "gr" 'diff-hl-revert-hunk
   "gd" 'magit-diff-buffer-file
