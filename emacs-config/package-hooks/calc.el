7ccba45d3242d63b03de7e3067c585b2ecd21de5
Liu Xiang <liuxiang@ktjr.com>
2017-10-09 21:18:32 +0800

    Revise kbds of calc-mode

--- emacs-config/package-hooks/calc.el
+++ emacs-config/package-hooks/calc.el
@@ -1,2 +1,7 @@
 (with-eval-after-load 'calc
+  (define-key calc-mode-map [M-tab] #'calc-roll-up)
+  (define-key calc-mode-map (kbd "M-k") #'lx/window-up-fallback-to-switch-frame))
+
+(with-eval-after-load 'calc-yank
+  (define-key calc-mode-map [M-tab] #'calc-roll-up)
   (define-key calc-mode-map (kbd "M-k") #'lx/window-up-fallback-to-switch-frame))
