b6e130c172830b961b6708a425158c07e63624d1
Liu Xiang <liuxiang@ktjr.com>
2018-05-07 00:00:09 +0800

    Extract aliases

---
+++ .zsh-aliases.zsh
@@ -0,0 +1,44 @@
+alias -- +x='chmod +x '
+alias -- '--=git checkout -'
+alias l1='ls -1'
+alias cdg='cd ~liuxiang/git-umeng'
+alias guse='rvm gemset use'
+alias gemls='rvm gemset list'
+alias ls="ls -G"
+alias grep="grep --color=auto"
+alias ggrep="ggrep --color=auto"
+alias tomcat="/usr/lib/tomcat/bin/catalina.sh run"
+alias s="screen -e^jj"
+alias -g ODX='| od -Ad -tx1'
+alias -g ODC='| od -Ad -tc'
+alias -s tgz='tar -zxf'
+alias -s tar.gz='tar -zxf'
+alias rvmdefault='rvm use default'
+alias k='kill'
+
+alias gls='gls --color --quoting-style=literal '
+alias lf='gls --color --quoting-style=literal -ldU '
+alias pn='print -l'
+alias a='cd ..'
+alias aa='cd ../../'
+alias aaa='cd ../../../'
+alias aaaa='cd ../../../../'
+alias aaaaa='cd ../../../../../'
+alias aaaaaa='cd ../../../../../../'
+alias aaaaaaa='cd ../../../../../../../'
+alias aaaaaaaa='cd ../../../../../../../../'
+alias aaaaaaaaa='cd ../../../../../../../../../'
+alias rspec='rspec -I. -fd --color'
+alias vih="sudo vim /etc/hosts"
+alias lv=lnav
+alias bslist='brew services list'
+alias bsstart='brew services start'
+alias bsstop='brew services stop'
+alias blist='brew list'
+alias bag='brew list | ag '
+alias bsearch='brew search'
+alias binstall='brew install'
+alias binfo='brew info'
+alias vg=vagrant
+alias pc='pc.sh'
+
