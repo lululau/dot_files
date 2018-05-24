b8f375564fae156c9856a31cd85ca4017aa17eba
Liu Xiang <liuxiang@ktjr.com>
2018-05-24 09:45:20 +0800

    Add kbd for lx/download-org-images for org-mode

--- emacs-config/package-hooks/org-mode.el
+++ emacs-config/package-hooks/org-mode.el
@@ -28,6 +28,7 @@
         (org-table-calc-current-TBLFM)
         (forward-line))))
 
+  (spacemacs/set-leader-keys-for-major-mode 'org-mode "DI" #'lx/download-org-images)
   (spacemacs/set-leader-keys-for-major-mode 'org-mode "tR" #'lx/org-table-recalculate-multi-formulas)
 
   (spacemacs/set-leader-keys-for-major-mode 'org-mode "oy" #'lx/yank-babel-src)
