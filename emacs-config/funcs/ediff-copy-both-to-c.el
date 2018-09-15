652530fd3321703172e951615e0a797f74a0b8a9
Liu Xiang <liuxiang@ktjr.com>
2018-09-15 21:49:10 +0800

    Modify kbd for 'ediff-copy-both-to-C

--- emacs-config/funcs/ediff-copy-both-to-c.el
+++ emacs-config/funcs/ediff-copy-both-to-c.el
@@ -4,4 +4,4 @@
                    (concat
                     (ediff-get-region-contents ediff-current-difference 'A ediff-control-buffer)
                     (ediff-get-region-contents ediff-current-difference 'B ediff-control-buffer))))
-(defun add-d-to-ediff-mode-map () (define-key ediff-mode-map (kbd "s-D") 'ediff-copy-both-to-C))
+(defun add-d-to-ediff-mode-map () (define-key ediff-mode-map (kbd "c") 'ediff-copy-both-to-C))
