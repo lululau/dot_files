3f36f77189226333bc9f291a7ffad66dd717a98b
Liu Xiang <liuxiang@ktjr.com>
2019-04-08 11:45:22 +0800

    Add kbd for Info-mode

--- emacs-config/package-hooks/Info-mode.el
+++ emacs-config/package-hooks/Info-mode.el
@@ -1,2 +1,5 @@
 (with-eval-after-load 'info
-  (define-key Info-mode-map (kbd "C-n") #'evil-search-next))
+  (define-key Info-mode-map (kbd "C-n") #'evil-search-next)
+  (define-key Info-mode-map (kbd "g") nil)
+  (define-key Info-mode-map (kbd "gg") #'evil-goto-first-line)
+  (define-key Info-mode-map (kbd "G") #'evil-goto-line))
