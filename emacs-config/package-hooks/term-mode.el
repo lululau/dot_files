86f080766ca3c334ea0a76f6e0c4436bdfcf5907
Liu Xiang <liuxiang@ktjr.com>
2020-09-01 12:52:57 +0800

    Revise kbds for vterm

--- emacs-config/package-hooks/term-mode.el
+++ emacs-config/package-hooks/term-mode.el
@@ -29,9 +29,9 @@
   (define-key vterm-mode-map (kbd "M-D") #'(lambda () (interactive) (comint-send-string (get-buffer-process (current-buffer)) "exit-program\n")))
 
   (let ((map (lookup-key vterm-mode-map "\e")))
-    (define-key map "h" #'evil-window-left)
-    (define-key map "l" #'evil-window-right)
-    (define-key map "j" #'lx/window-down-fallback-to-switch-frame)
+    ;; (define-key map "h" #'evil-window-left)
+    ;; (define-key map "l" #'evil-window-right)
+    ;; (define-key map "j" #'lx/window-down-fallback-to-switch-frame)
     (define-key map "k" #'lx/window-up-fallback-to-switch-frame)
     (define-key map "H" #'evil-window-move-far-left)
     (define-key map "J" #'evil-window-move-very-bottom)
