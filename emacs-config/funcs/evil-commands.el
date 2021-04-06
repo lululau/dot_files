f82f01047b0815d87095cf2c60525a962bfb0f6d
Liu Xiang <liuxiang@ktjr.com>
2021-04-06 13:28:15 +0800

    Add replace-example-to-src

--- emacs-config/funcs/evil-commands.el
+++ emacs-config/funcs/evil-commands.el
@@ -2,3 +2,10 @@
   (interactive)
   (let ((evil-ex-current-buffer (current-buffer)))
     (evil-ex-execute "g/:PROPERTIES:/.,/:END:/d")))
+
+
+(defun vi/convert-org-example-to-src (lang)
+  (let ((evil-ex-current-buffer (current-buffer)))
+    (evil-ex-execute (format "%%s/#\\+begin_example\\C/#+begin_src %s" lang))
+    (evil-ex-execute (format "%%s/#\\+BEGIN_EXAMPLE\\C/#+BEGIN_SRC %s" lang))
+    (evil-ex-execute "%s/#\\+end_example/#+end_src" )))
