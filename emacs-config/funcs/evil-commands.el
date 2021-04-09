946708dba369e327bf2b94ca7915193433a92173
Liu Xiang <liuxiang@ktjr.com>
2021-04-09 14:03:52 +0800

    Revise interactive specification for vi/convert-org-example-to-src

--- emacs-config/funcs/evil-commands.el
+++ emacs-config/funcs/evil-commands.el
@@ -4,10 +4,9 @@
     (evil-ex-execute "g/:PROPERTIES:/.,/:END:/d")))
 
 
-(defun vi/convert-org-example-to-src ()
-  (interactive)
-  (let ((lang (read-string "Language: "))
-        (evil-ex-current-buffer (current-buffer)))
+(defun vi/convert-org-example-to-src (lang)
+  (interactive "sLanguage: ")
+  (let ((evil-ex-current-buffer (current-buffer)))
     (evil-ex-execute (format "%%s/#\\+begin_example\\C/#+begin_src %s" lang))
     (evil-ex-execute (format "%%s/#\\+BEGIN_EXAMPLE\\C/#+BEGIN_SRC %s" lang))
     (evil-ex-execute "%s/#\\+end_example/#+end_src" )))
