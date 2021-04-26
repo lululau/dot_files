db851c5433fca0d7e0c460fd576195bbf6e4d474
Liu Xiang <liuxiang@ktjr.com>
2021-04-26 23:30:48 +0800

    Fix bug

--- emacs-config/crafts/run-in-vterm.el
+++ emacs-config/crafts/run-in-vterm.el
@@ -255,7 +255,7 @@
 
 (defun helm-vterm-arql-option-list ()
   (mapcar (lambda (host) (cons host host))
-          (s-split "\n" (shell-command-to-string "perl -ne 'unless (/^default/) { if (/^\\w/) {s/:$//; print;}}' ~/.arql.d/init.yml") t)))
+          (s-split "\n" (shell-command-to-string "perl -ne 'if (/^\\w.*:\\s*$/) {s/:$//; print;}' ~/.arql.d/init.yml") t)))
 
 (defun helm-vterm-arql-run (env)
   (let ((cmd  (format "~/.rvm/gems/ruby-3.0.0/bin/arql -e %s" env))
