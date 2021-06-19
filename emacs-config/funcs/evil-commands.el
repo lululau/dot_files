d819741e7e6bc5be6c4427816ae67cc55e0515b4
Liu Xiang <liuxiang@ktjr.com>
2021-06-19 20:53:42 +0800

    Add vi/strip-ansi-code

--- emacs-config/funcs/evil-commands.el
+++ emacs-config/funcs/evil-commands.el
@@ -10,3 +10,8 @@
     (evil-ex-execute (format "%%s/#\\+begin_example\\C/#+begin_src %s" lang))
     (evil-ex-execute (format "%%s/#\\+BEGIN_EXAMPLE\\C/#+BEGIN_SRC %s" lang))
     (evil-ex-execute "%s/#\\+end_example/#+end_src" )))
+
+(defun vi/strip-ansi-code ()
+  (interactive)
+  (let ((evil-ex-current-buffer (current-buffer)))
+    (evil-ex-execute  "%s/\[[0-9;]*m//g")))
