222591334b14f40acaae962b789ed2cf0b7ce0a1
Liu Xiang <liuxiang@ktjr.com>
2022-04-13 12:50:25 +0800

    Update kbd for force-update-mu4e

--- emacs-config/package-hooks/mu4e.el
+++ emacs-config/package-hooks/mu4e.el
@@ -2,4 +2,4 @@
   (setcdr (plist-get (cdr (assoc 'delete mu4e-marks)) :char) "×"))
 
 (with-eval-after-load 'mu4e-main
-  (define-key mu4e-main-mode-map (kbd "g r") 'lx/force-update-mu4e))
+  (define-key mu4e-main-mode-map (kbd "s-R") 'lx/force-update-mu4e))
