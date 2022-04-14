fe127a86489a13ffd550eb2d90d00f694b25b8ec
Liu Xiang <liuxiang@ktjr.com>
2022-04-14 11:36:14 +0800

    Revise SPC o s s

--- emacs-config/funcs/save-scratch.el
+++ emacs-config/funcs/save-scratch.el
@@ -2,7 +2,8 @@
   (interactive)
   (let* ((now (format-time-string "%Y%m%d-%H%M%S" (current-time)))
          (file (format "~/Documents/materials/scratches/scratch-%s.txt" now))
+         (current-buffer (current-buffer))
          (buffer (find-file file)))
-    (with-current-buffer "*scratch*"
+    (with-current-buffer current-buffer
       (copy-to-buffer buffer (beginning-of-buffer) (end-of-buffer)))
     (save-buffer)))
