1900606dbaec919d8258ff8af8de1f9ebf6b64a8
Liu Xiang <liuxiang@ktjr.com>
2022-04-15 22:35:36 +0800

    Revise all-buffer-completion

--- emacs-config/crafts/all-buffer-completion.el
+++ emacs-config/crafts/all-buffer-completion.el
@@ -4,14 +4,16 @@
 
 (defun helm-all-buffer-completion--all-emacs-buffer-lines (word)
   (mapcan (lambda (buf)
-               (with-current-buffer buf
-                 (let* ((content (buffer-substring-no-properties (point-min) (point-max)))
-                        (lines (s-lines content)))
-                   (seq-filter (lambda (line) (s-contains-p word line)) lines))))
-             (buffer-list)))
+            (if (not (string-prefix-p "*tmux" (buffer-name buf)))
+                (with-current-buffer buf
+                  (let* ((content (buffer-substring-no-properties (point-min) (point-max)))
+                         (lines (s-lines content)))
+                    (seq-filter (lambda (line) (s-contains-p word line)) lines)))))
+          (buffer-list)))
 
 (defun helm-all-buffer-completion--all-tmux-buffer-lines (word)
-  (s-lines (shell-command-to-string (format "%s/capture-all-tmux-panes.sh '%s'" helm-all-buffer-completion-script-dir word))))
+  (s-lines (shell-command-to-string
+            (format "%s/capture-all-tmux-panes.sh '%s'" helm-all-buffer-completion-script-dir word))))
 
 (defun helm-all-buffer-completion--all-lines (word)
   (append (helm-all-buffer-completion--all-emacs-buffer-lines word)
@@ -23,19 +25,22 @@
            (word-at-point (car (last (s-split-words current-line)))))
       (when word-at-point
         (let* ((WORD-at-point (car (last (split-string current-line "\\s-+" t))))
-               (vterm-lines (helm-all-buffer-completion--all-lines word-at-point))
-               (vterm-WORDS (mapcan (lambda (line) (split-string line "\\s-+" t)) vterm-lines))
-               (vterm-words (mapcan (lambda (WORD) (s-split-words WORD)) vterm-WORDS))
-               (candidates (append vterm-lines vterm-WORDS vterm-words))
-               (candidates (let (result)(dolist (candidate candidates result)
-                                          (when (string-prefix-p word-at-point candidate)
-                                            (setq result (cons (cons candidate (cons candidate word-at-point)) result)))
-                                          (when (string-prefix-p WORD-at-point candidate)
-                                            (setq result (cons (cons candidate (cons candidate WORD-at-point)) result)))
-                                          (let ((idx (s-index-of word-at-point candidate)))
-                                            (when idx
-                                              (let ((sub-candidate (substring candidate idx)))
-                                                (setq result (cons (cons sub-candidate (cons sub-candidate word-at-point)) result)))))))))
+               (lines (helm-all-buffer-completion--all-lines word-at-point))
+               (WORDS (mapcan (lambda (line) (split-string line "\\s-+" t)) lines))
+               (words (mapcan (lambda (WORD) (s-split-words WORD)) WORDS))
+               (candidates (append lines WORDS words))
+               (candidates
+                (let (result)
+                  (dolist (candidate candidates result)
+                    (when (string-prefix-p word-at-point candidate)
+                      (setq result (cons (cons candidate (cons candidate word-at-point)) result)))
+                    (when (string-prefix-p WORD-at-point candidate)
+                      (setq result (cons (cons candidate (cons candidate WORD-at-point)) result)))
+                    (let ((idx (s-index-of word-at-point candidate)))
+                      (when idx
+                        (let ((sub-candidate (substring candidate idx)))
+                          (setq result
+                                (cons (cons sub-candidate (cons sub-candidate word-at-point)) result)))))))))
           (seq-uniq (seq-sort (lambda (a b) (< (length (car a)) (length (car b)))) candidates)))))))
 
 (defun helm-all-buffer-completion-insert (selected)
@@ -55,7 +60,7 @@
    (keymap :initform helm-map)))
 
 (setq helm-all-buffer-completion-list
-      (helm-make-source "vterm completion" 'helm-all-buffer-completion-source))
+      (helm-make-source "All Buffer (Emacs and Tmux buffers) Completion" 'helm-all-buffer-completion-source))
 
 (defun all-buffer-completion ()
   (interactive)
