0b98ad0a9d5bb405c4eaba3013eff7a33a6c77f3
Liu Xiang <liuxiang@ktjr.com>
2022-05-09 14:35:35 +0800

    Fix bug of viewing mu4e msg via xwidget

--- emacs-config/funcs/xwidgets-integration.el
+++ emacs-config/funcs/xwidgets-integration.el
@@ -1,6 +1,13 @@
+(defun xwidget-webkit-browse-mu4e-msg (url &rest args)
+  (require 'xwidget)
+  (if (buffer-live-p xwidget-webkit-last-session-buffer)
+      (switch-to-buffer xwidget-webkit-last-session-buffer))
+  (xwidget-webkit-browse-url url)
+  (switch-to-buffer xwidget-webkit-last-session-buffer))
+
 (defun my-mu4e-action-view-with-xwidget (msg)
   "View the body of the message inside xwidget-webkit."
   (unless (fboundp 'xwidget-webkit-browse-url)
     (mu4e-error "No xwidget support available"))
-  (xwidget-webkit-browse-url (concat "file://" (mu4e~write-body-to-html msg)) t))
-
+  (let ((browse-url-browser-function 'xwidget-webkit-browse-mu4e-msg))
+    (mu4e-action-view-in-browser msg)))
