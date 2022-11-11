91ec9d32ae9f08a8588d846c5f693e811c842064
Liu Xiang <liuxiang@ktjr.com>
2022-11-11 20:56:43 +0800

    Work around projectile bug

--- emacs-config/advices/projectile.el
+++ emacs-config/advices/projectile.el
@@ -5,3 +5,11 @@
         (apply origfunc args))))
 
 (advice-add 'projectile-project-root :around #'projectile-project-root-remote)
+
+(with-eval-after-load 'projectile
+  (defun projectile-root-bottom-up (dir &optional list)
+    "Identify a project root in DIR by bottom-up search for files in LIST.
+If LIST is nil, use `projectile-project-root-files-bottom-up' instead.
+Return the first (bottommost) matched directory or nil if not found."
+    (cl-some (lambda (name) (projectile-locate-dominating-file dir name))
+             (or list projectile-project-root-files-bottom-up))))
