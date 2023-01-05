cff28246a25431aa08dc6c84034434b7277442f1
Liu Xiang <liuxiang@ktjr.com>
2023-01-05 16:08:16 +0800

    Change default colorscheme to solarized-light.

--- ptpython-config.py
+++ ptpython-config.py
@@ -100,7 +100,7 @@ def configure(repl):
     repl.enable_input_validation = True
 
     # Use this colorscheme for the code.
-    repl.use_code_colorscheme('monokai')
+    repl.use_code_colorscheme('solarized-light')
 
     # Enable 24bit True color. (Not all terminals support this. -- maybe check
     # $TERM before changing.)
