3de2a73d1a7e03f92ec25bffe5eb2534be4813eb
Liu Xiang <liuxiang@ktjr.com>
2023-08-29 22:45:26 +0800

    Fix bug of regular file styles

--- xplr/init.lua
+++ xplr/init.lua
@@ -3231,11 +3231,55 @@ xplr.fn.builtin.fmt_general_table_row_cols_0 = function(m)
 end
 
 -- Renders the second column in the table
+-- xplr.fn.builtin.fmt_general_table_row_cols_1 = function(m)
+--   local nl = xplr.util.paint("\\n", { add_modifiers = { "Italic", "Dim" } })
+--   local r = m.tree .. m.prefix
+--   local style = xplr.util.lscolor(m.absolute_path)
+--   style = xplr.util.style_mix({ style, m.style })
+
+--   if m.meta.icon == nil then
+--     r = r .. ""
+--   else
+--     r = r .. m.meta.icon .. " "
+--   end
+
+--   local rel = m.relative_path
+--   if m.is_dir then
+--     rel = rel .. "/"
+--   end
+--   r = r .. xplr.util.paint(xplr.util.shell_escape(rel), style)
+
+--   r = r .. m.suffix .. " "
+
+--   if m.is_symlink then
+--     r = r .. "-> "
+
+--     if m.is_broken then
+--       r = r .. "×"
+--     else
+--       local symlink_path =
+--           xplr.util.shorten(m.symlink.absolute_path, { base = m.parent })
+--       if m.symlink.is_dir then
+--         symlink_path = symlink_path .. "/"
+--       end
+--       r = r .. symlink_path:gsub("\n", nl)
+--     end
+--   end
+
+--   return r
+-- end
+
 xplr.fn.builtin.fmt_general_table_row_cols_1 = function(m)
   local nl = xplr.util.paint("\\n", { add_modifiers = { "Italic", "Dim" } })
   local r = m.tree .. m.prefix
-  local style = xplr.util.lscolor(m.absolute_path)
-  style = xplr.util.style_mix({ style, m.style })
+
+  -- start of edited part
+
+  local style = m.style
+  -- local style = xplr.util.lscolor(m.absolute_path)
+  -- style = xplr.util.style_mix({ style, m.style })
+
+  -- end of edited part
 
   if m.meta.icon == nil then
     r = r .. ""
@@ -3258,7 +3302,7 @@ xplr.fn.builtin.fmt_general_table_row_cols_1 = function(m)
       r = r .. "×"
     else
       local symlink_path =
-          xplr.util.shorten(m.symlink.absolute_path, { base = m.parent })
+        xplr.util.shorten(m.symlink.absolute_path, { base = m.parent })
       if m.symlink.is_dir then
         symlink_path = symlink_path .. "/"
       end
