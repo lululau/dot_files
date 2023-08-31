0158c2921c5b2a43abb5c6b719b05dc489dee500
Liu Xiang <liuxiang@ktjr.com>
2023-08-31 16:50:31 +0800

    Add tree-view plugin

--- xplr/init.lua
+++ xplr/init.lua
@@ -3681,6 +3681,64 @@ os.execute(
 require("xpm").setup({
     'dtomvan/xpm.xplr',
     {
+      "sayanarijit/tree-view.xplr",
+      setup = function ()
+        require("tree-view").setup({
+            mode = "switch_layout",
+            key = "T",
+
+            -- If you feel slowness, you might want to toggle back to the default view.
+            toggle_layout_mode = "default",
+            toggle_layout_key = "T",
+
+            -- Press backspace to close all and back and close
+            close_all_and_back_mode = "default",
+            close_all_and_back_key = "backspace",
+
+            -- Toggle expansion without entering
+            toggle_expansion_mode = "default",
+            toggle_expansion_key = "tab",
+
+            -- Toggle expansion of all the nodes under pwd
+            toggle_expansion_all_mode = "default",
+            toggle_expansion_all_key = "O",
+
+            -- Focus on the next visible line, not compatible with up/down action
+            focus_next_mode = "default",
+            focus_next_key = "]",
+
+            -- Focus on the previous visible line, not compatible with up/down action
+            focus_prev_mode = "default",
+            focus_prev_key = "[",
+
+            -- Go to the next deep level directory that's open
+            goto_next_open_mode = "default",
+            goto_next_open_key = ")",
+
+            -- Go to the previous deep level directory that's open
+            goto_prev_open_mode = "default",
+            goto_prev_open_key = "(",
+
+            -- Whether to display the tree in full screen
+            fullscreen = false,
+
+            -- Indent for the branches of the tree
+            indent = "  ",
+
+            -- Start xplr with tree view layout
+            as_initial_layout = false,
+
+            -- Disables toggling layout.
+            as_default_layout = false,
+
+            -- Automatically fallback to this layout for better performance if the
+            -- branch contains # of nodes more than the threshold value
+            fallback_layout = "Table",
+            fallback_threshold = 500,  -- default: nil (disabled)
+        })
+      end
+    },
+    {
       "sayanarijit/registers.xplr",
       setup = function ()
         require("registers").setup{
