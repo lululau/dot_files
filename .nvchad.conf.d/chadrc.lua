6eae177c405af6549770d4d6b61c7b7ad56c5121
Liu Xiang <liuxiang@ktjr.com>
2023-09-25 10:40:48 +0800

    Show dashboard on startup

--- .nvchad.conf.d/chadrc.lua
+++ .nvchad.conf.d/chadrc.lua
@@ -10,6 +10,22 @@ M.ui = {
 
   hl_override = highlights.override,
   hl_add = highlights.add,
+
+  tabufline = {
+    show_numbers = true,
+  },
+
+  nvdash = {
+    load_on_startup = true,
+    buttons = {
+      { "󰍉  Find File", "Spc f f", "Telescope find_files" },
+      { "󰈚  Recent Files", "Spc f o", "Telescope oldfiles" },
+      { "󰈭  Find Word", "Spc f w", "Telescope live_grep" },
+      { "  Bookmarks", "Spc m a", "Telescope marks" },
+      { "  Themes", "Spc t h", "Telescope themes" },
+      { "  Mappings", "Spc c h", "NvCheatsheet" },
+    },
+  }
 }
 
 M.plugins = "custom.plugins"
