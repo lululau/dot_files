c056c879830b00a805a73013c39093d7b00afc93
Liu Xiang <liuxiang@ktjr.com>
2023-09-25 13:07:00 +0800

    Add keybindings for alt+number to switch tabs

--- .nvchad.conf.d/misc/init.lua
+++ .nvchad.conf.d/misc/init.lua
@@ -27,3 +27,15 @@ vim.api.nvim_set_keymap("", "<D-7>", ":call SelectTab(7)<CR>", { noremap = true,
 vim.api.nvim_set_keymap("", "<D-8>", ":call SelectTab(8)<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("", "<D-9>", ":call SelectTab(9)<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("", "<D-0>", ":call SelectTab(0)<CR>", { noremap = true, silent = true })
+
+
+vim.api.nvim_set_keymap("", "<A-1>", ":call SelectTab(1)<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("", "<A-2>", ":call SelectTab(2)<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("", "<A-3>", ":call SelectTab(3)<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("", "<A-4>", ":call SelectTab(4)<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("", "<A-5>", ":call SelectTab(5)<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("", "<A-6>", ":call SelectTab(6)<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("", "<A-7>", ":call SelectTab(7)<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("", "<A-8>", ":call SelectTab(8)<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("", "<A-9>", ":call SelectTab(9)<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("", "<A-0>", ":call SelectTab(0)<CR>", { noremap = true, silent = true })
