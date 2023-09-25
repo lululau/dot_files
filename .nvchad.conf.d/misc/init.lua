a89deeae63b825898a0f0f5b10660aa22d189390
Liu Xiang <liuxiang@ktjr.com>
2023-09-25 17:29:59 +0800

    Bind <A-1> to <A-9> to switch to the corresponding window

--- .nvchad.conf.d/misc/init.lua
+++ .nvchad.conf.d/misc/init.lua
@@ -17,3 +17,11 @@ vim.cmd("highlight CopilotSuggestion guifg=#24ead9 ctermfg=6")
 vim.cmd("au TextYankPost * silent! lua vim.highlight.on_yank()")
 
 vim.g.vim_textobj_parameter_mapping = 'a'
+
+
+if vim.g.neovide then
+  for i = 1, 9 do
+    vim.api.nvim_set_keymap('n', '<A-'..i..'>', ':'..i .. 'wincmd w<cr>', {noremap = true, silent = true})
+    vim.api.nvim_set_keymap('i', '<C-o><A-'..i..'>', ':'..i .. 'wincmd w', {noremap = true, silent = true})
+   end
+end
