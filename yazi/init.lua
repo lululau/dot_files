685f999267986c66e2e43976fd7fd315136f23c7
Liu Xiang <liuxiang921@gmail.com>
2024-12-09 20:56:53 +0800

    Update yazi config for Yazi v0.4

--- yazi/init.lua
+++ yazi/init.lua
@@ -4,7 +4,7 @@ require("starship"):setup()
 
 function Linemode:full()
 	local year = os.date("%Y")
-	local time = (self._file.cha.modified or 0) // 1
+	local time = (self._file.cha.mtime or 0) // 1
 
 	if time > 0 and os.date("%Y", time) == year then
 		time = os.date("%b %d %H:%M", time)
@@ -12,7 +12,7 @@ function Linemode:full()
 		time = time and os.date("%b %d  %Y", time) or ""
 	end
 
-  local perms = self._file.cha:permissions() or ""
+  local perms = self._file.cha:perm() or ""
 	local size = self._file:size()
 	return ui.Line(string.format("%s %s %s ", perms, size and ya.readable_size(size) or "-", time))
 end
