a2d24e73111fa98afc7e62f3a1ffe0fb77f2aed9
Liu Xiang <liuxiang921@gmail.com>
2025-04-08 19:50:26 +0800

    Bind super-w to quit helm buffer
    - Add keybinding for `s-w` to quit helm buffer using `helm-keyboard-quit`
    - Update helm buffer map to include new keybinding for better usability
    - Maintain existing super-return binding for consistent cross-platform behavior

--- emacs-config/package-hooks/bind-super-return-for-helm-keymaps.el
+++ emacs-config/package-hooks/bind-super-return-for-helm-keymaps.el
@@ -3,7 +3,8 @@
 
 (with-eval-after-load 'helm-buffers
 ;  (define-key helm-buffers-ido-virtual-map (kbd (if (display-graphic-p) "<s-return>" "s-RET"))   'helm-ff-run-switch-other-window)
-  (define-key helm-buffer-map (kbd (if (display-graphic-p) "<s-return>" "s-RET"))     'helm-buffer-switch-other-window))
+  (define-key helm-buffer-map (kbd (if (display-graphic-p) "<s-return>" "s-RET"))     'helm-buffer-switch-other-window)
+  (define-key helm-buffer-map (kbd "s-w") 'helm-keyboard-quit))
 
 (with-eval-after-load 'helm-grep
   (define-key helm-grep-map (kbd (if (display-graphic-p) "<s-return>" "s-RET")) 'helm-grep-run-other-window-action))
