e645f9b14e5d5222145e3885b65ad050386feba1
Liu Xiang <liuxiang921@gmail.com>
2025-04-20 20:39:33 +0800

    Add git commit to org-journal integration in Emacs config
    - Introduce `lx/git-commit-to-org-journal` function for linking commits to journal
    - Bind `C-c C-j` key to new function in magit-status and magit-log modes
    - Duplicate function definition in both magit.el and git-commit-to-org-journal.el for consistency
    - Automatically set ":Git:" tag when creating new journal entry from commit

--- emacs-config/package-hooks/git-commit.el
+++ emacs-config/package-hooks/git-commit.el
@@ -1,4 +1,8 @@
 (with-eval-after-load 'magit-status
   (require 'gpt-commit)
+  (define-key magit-status-mode-map (kbd "C-c C-j") 'lx/git-commit-to-org-journal)
   (define-key git-commit-mode-map (kbd "C-c C-m") 'gpt-commit-message)
   (define-key git-commit-mode-map (kbd "C-c C-g") 'gpt-commit-message))
+
+(with-eval-after-load 'magit-log
+  (define-key magit-log-mode-map (kbd "C-c C-j") 'lx/git-commit-to-org-journal))
--- emacs-config/package-hooks/magit.el
+++ emacs-config/package-hooks/magit.el
@@ -36,6 +36,14 @@ Like `vc-mode-line' but simpler, more efficient, and less buggy."
   (add-hook 'magit-post-checkout-hooks 'magit-refresh-projectile-buffers-vc-mode-line))
 
 (with-eval-after-load 'magit-mode
+  (defun lx/git-commit-to-org-journal ()
+    (interactive)
+    (evil-visual-line)
+    (call-interactively 'org-store-link)
+    (evil-exit-visual-state)
+    (org-journal-new-entry nil)
+    (org-insert-all-links 1 "" "")
+    (org-set-tags ":Git:"))
   (define-key magit-mode-map (kbd "S-SPC") nil)
   (define-key magit-mode-map [S-tab] 'magit-section-cycle-global)
   (define-key magit-mode-map [remap org-store-link] 'orgit-store-link))
