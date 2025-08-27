c350f7f2e3773e29725cb714cf1f85788a9366fa
Liu Xiang <liuxiang921@gmail.com>
2025-08-27 10:42:15 +0800

    Replace lazygit with magit (a script wrapped nvim+neogit.nvim) plugin

---
+++ yazi/plugins/magit.yazi/main.lua
@@ -0,0 +1,31 @@
+return {
+    entry = function()
+        local output = Command("git"):arg("status"):stderr(Command.PIPED):output()
+        if output.stderr ~= "" then
+            ya.notify({
+                title = "Magit",
+                content = "Not in a git directory\nError: " .. output.stderr,
+                level = "warn",
+                timeout = 5,
+            })
+        else
+            permit = ya.hide()
+            local status, err = Command("magit")
+							:stdin(Command.INHERIT)
+							:stdout(Command.INHERIT)
+							:stderr(Command.INHERIT)
+							:spawn():wait()
+            if not status then
+              ya.notify({
+                  title = "Magit",
+                  content = "Failed to run magit (code : " .. err .. ")",
+                  level = "error",
+                  timeout = 5,
+              })
+            else
+              permit:drop()
+              ya.manager_emit('escape', {visual = true, select = true})
+            end
+        end
+    end,
+}
