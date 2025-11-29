65daa780b52d6a661c08da67442e8f151abc1546
Liu Xiang <liuxiang921@gmail.com>
2025-11-29 20:50:35 +0800

    Make SPC-aojo to move TODOs of previous journals to today's

--- emacs-config/funcs/org-journal-find-today-entry.el
+++ emacs-config/funcs/org-journal-find-today-entry.el
@@ -1,10 +1,7 @@
 ;;;###autoload
 (defun org-journal-find-today-entry ()
   (interactive)
-  (require 'org-journal)
-  (org-journal--sanity-checks)
-  (org-journal--create-journal-dir)
-  (funcall org-journal-find-file (org-journal--get-entry-path nil)))
+  (org-journal-new-entry t (current-time) t))
 
 
 ;;;###autoload
