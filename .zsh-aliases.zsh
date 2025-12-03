ca24c58592ae2a545b88a3e390e7c397f728e69f
Liu Xiang <liuxiang921@gmail.com>
2025-12-03 19:46:51 +0800

    Add J global alias for piping to jless
    - Add `J` global alias to pipe output to `jless` for JSON formatting and viewing
    - Maintain consistency with existing pipeline aliases like `G`, `C`, and `LL`

--- .zsh-aliases.zsh
+++ .zsh-aliases.zsh
@@ -50,6 +50,7 @@ alias -g V="| view"
 alias -g C='| wc -l'
 alias -g LL="2>&1 | view"
 alias -g G='| rg'
+alias -g J='| jless'
 alias -g P='| pbcopy'
 alias pp='pbpaste | view'
 alias pj='pbpaste | jless'
