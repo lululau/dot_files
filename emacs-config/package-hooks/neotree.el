6ddd6301d87e105653c4b353b7c8740ade57e145
Liu Xiang <liuxiang921@gmail.com>
2026-05-30 23:00:26 +0800

    调整 neotree 模式行高度以匹配 spaceline 的 powerline 高度
    - 调整 neotree 模式行高度，使其与 spaceline 的 `powerline-height` 保持一致
    - 在 neotree 模式行末尾添加透明 XPM 图像作为高度适配的 spacer
    - 仅在 `powerline-height` 已定义且非 nil 时启用该适配逻辑
    - 使用 `setq-local` 确保 `mode-line-format` 修改作用于当前缓冲区

--- emacs-config/package-hooks/neotree.el
+++ emacs-config/package-hooks/neotree.el
@@ -6,4 +6,22 @@
   (define-key neotree-mode-map (kbd "s-T") #'(lambda () (interactive) (select-window-by-number 1) (split-window-below-and-focus)))
   (define-key neotree-mode-map (kbd "s-n") #'(lambda () (interactive) (select-window-by-number 1) (switch-to-buffer-other-window (generate-new-buffer "*Untitled*")) (undo-tree-mode)))
   (define-key neotree-mode-map (kbd "s-N") #'(lambda () (interactive) (select-window-by-number 1) (switch-to-buffer (generate-new-buffer "*Untitled*")) (undo-tree-mode)))
-  (define-key neotree-mode-map [tab] #'(lambda () (interactive) (neo-buffer--toggle-expand (neo-buffer--get-filename-current-line)) (neo-buffer--refresh t))))
+  (define-key neotree-mode-map [tab] #'(lambda () (interactive) (neo-buffer--toggle-expand (neo-buffer--get-filename-current-line)) (neo-buffer--refresh t)))
+
+  ;; Make neotree mode-line height match spaceline by appending a transparent
+  ;; XPM spacer image whose height equals `powerline-height'.
+  (add-hook 'neotree-mode-hook
+            (lambda ()
+              (when (and (boundp 'powerline-height)
+                         powerline-height
+                         mode-line-format)
+                (let* ((h powerline-height)
+                       (pixel-rows (mapconcat (lambda (_) " ")
+                                              (make-list h nil) "\",\""))
+                       (xpm-data (format "/* XPM */ static char * s[] = {\"1 %d 1 1\",\"  c None\",\"%s\"};"
+                                         h pixel-rows))
+                       (spacer (propertize
+                                " " 'display
+                                (list 'image :type 'xpm :data xpm-data :ascent 'center))))
+                  (setq-local mode-line-format
+                              (append mode-line-format (list spacer))))))))
