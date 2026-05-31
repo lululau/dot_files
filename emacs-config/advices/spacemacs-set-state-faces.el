970f0c4b027053cf15e17a67caeefa7eac5fc969
Liu Xiang <liuxiang921@gmail.com>
2026-05-31 11:42:11 +0800

    更新 Magit 哈希显示颜色为薰衣草色以增强可读性
    - 将 magit-hash 面的颜色从 subtext0 改为 #91acee 薰衣草色，避免与默认文本色过于接近
    - 添加注释说明颜色调整原因，提升代码可维护性

--- emacs-config/advices/spacemacs-set-state-faces.el
+++ emacs-config/advices/spacemacs-set-state-faces.el
@@ -117,6 +117,8 @@
     (face-spec-set 'magit-log-head-label-tags '((t (:background "#c6a0f6" :foreground "#24273a" :inherit bold))))
     (face-spec-set 'magit-log-head-label-wip '((t (:background "#8bd5ca" :foreground "#24273a" :inherit bold))))
     (face-spec-set 'magit-log-sha1 '((t (:foreground "#8bd5ca"))))
+    ;; Magit hash: lavender instead of subtext0 (too close to default text)
+    (face-spec-set 'magit-hash '((t (:foreground "#91acee"))))
     ;; Magit misc
     (face-spec-set 'magit-diff-file-heading-highlight '((t (:background "#363a4f" :foreground "#8087a2"))))
     (face-spec-set 'magit-item-highlight '((t (:background "#1e2030" :extend t))))
