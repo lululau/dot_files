1511a3bf73e4b4fbfda8a685a1e7947fc55a1b34
Liu Xiang <liuxiang921@gmail.com>
2026-08-18 10:27:05 +0800

    优化 lsp-headerline 面包屑图标尺寸与边距
    - 为 lsp-icons--fix-image-background 添加 advice 支持自定义图标尺寸与外边距
    - 新增 lx/lsp-headerline-icon-size 与 lx/lsp-headerline-icon-margin 配置项
    - 默认将 headerline 图标缩小至 16px 并增加留白，改善视觉协调性

--- emacs-config/package-hooks/lsp-mode.el
+++ emacs-config/package-hooks/lsp-mode.el
@@ -3,3 +3,35 @@
 (with-eval-after-load 'lsp-mode
   (define-key lsp-mode-map (kbd "<M-return>") #'lsp-execute-code-action)
   (define-key lsp-mode-map (kbd "<S-return>") 'lsp-find-references))
+
+(with-eval-after-load 'lsp-icons
+  (defcustom lx/lsp-headerline-icon-size 16
+    "Size (width and height in pixels) for lsp-headerline breadcrumb icons."
+    :type 'integer
+    :group 'lsp-icons)
+
+  (defcustom lx/lsp-headerline-icon-margin '(2 . 1)
+    "Margin (padding) for lsp-headerline breadcrumb icons.
+Can be an integer or a cons cell (X . Y) specifying horizontal and vertical margins."
+    :type '(choice integer (cons integer integer))
+    :group 'lsp-icons)
+
+  (defun lx/lsp-icons--fix-image-background-advice (orig-fn image)
+    "Adjust size and margin for lsp-headerline breadcrumb icons."
+    (if image
+        (let ((display-image (get-text-property 0 'display image)))
+          (if (and (listp display-image)
+                   (plist-member (cl-copy-list (cl-rest display-image)) :type))
+              (let ((props (cl-copy-list (cl-rest display-image))))
+                (setq props (plist-put props :background (face-attribute 'header-line :background nil t)))
+                (when lx/lsp-headerline-icon-size
+                  (setq props (plist-put props :width lx/lsp-headerline-icon-size))
+                  (setq props (plist-put props :height lx/lsp-headerline-icon-size)))
+                (when lx/lsp-headerline-icon-margin
+                  (setq props (plist-put props :margin lx/lsp-headerline-icon-margin)))
+                (propertize " " 'display (cl-list* 'image props)))
+            (funcall orig-fn image)))
+      ""))
+
+  (advice-add 'lsp-icons--fix-image-background :around #'lx/lsp-icons--fix-image-background-advice))
+
